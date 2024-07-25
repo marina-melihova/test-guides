959847b39ad0d434b132847c5c55d2a5607f08a5
Maryna Melikhova <maryna_melikhova@epam.com>
2024-07-25 12:18:13 +0300

    block search engines crawlers

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -42,21 +42,8 @@ export default function Document() {
   return (
     <Html className="antialiased [font-feature-settings:'ss01']" lang="en">
       <Head>
+        <meta name="robots" content="noindex" />
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          `,
-          }}
-        />
       </Head>
       <body className="bg-white dark:bg-slate-900">
         <Main />
